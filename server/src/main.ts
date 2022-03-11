8de5265e55da2b872bf958af9792eed0224aa8a7
Art <4arturas@gmail.com>
2022-03-11 17:59:04 +0200

    Remove commented stuff

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,3 @@
-/*import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();*/
-
-
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
