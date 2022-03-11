c87997907b656f9505337306df30ee33607aa6b9
Art <4arturas@gmail.com>
2022-03-11 13:30:11 +0200

    Add .env file and import values from it

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,18 +5,28 @@ import { UsersModule } from './users/users.module';
 import { EventsModule } from './events/events.module';
 import { DatabaseModule } from './database/database.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {User} from "./users/entities/user.entity";
+import {Event} from "./events/entities/event.entity";
+require('dotenv').config();
+
+const POSTGRES_HOST = process.env.POSTGRES_HOST;
+const POSTGRES_PORT = parseInt(process.env.POSTGRES_PORT)
+const POSTGRES_USER = process.env.POSTGRES_USER
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD
+const POSTGRES_DATABASE = process.env.POSTGRES_DATABASE;
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '192.168.49.2',
-      port: 30432,
-      username: 'postgres',
-      password: 'PleaseChangeMe',
-      database: 'postgres',
-      entities: [],
+      host: POSTGRES_HOST,
+      port: POSTGRES_PORT,
+      username: POSTGRES_USER,
+      password: POSTGRES_PASSWORD,
+      database: POSTGRES_DATABASE,
+      entities: [User, Event],
       synchronize: true,
+      logging: false
     }),
       UsersModule, EventsModule, DatabaseModule
   ],
