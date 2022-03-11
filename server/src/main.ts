56f85c47bcf09e5073b9d009e435e1ad6e4f3c48
Art <4arturas@gmail.com>
2022-03-11 18:01:18 +0200

    Change API description

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,10 +11,10 @@ async function bootstrap() {
   });
 
   const config = new DocumentBuilder()
-      .setTitle('Cats example')
-      .setDescription('The cats API description')
+      .setTitle('Didomi example')
+      .setDescription('Didomi API description')
       .setVersion('1.0')
-      .addTag('cats')
+      .addTag('didomi')
       .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
