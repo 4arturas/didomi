b0d19d73bf2ffb6a2dcc4b32af9dccad1bd0236f
Art <4arturas@gmail.com>
2022-03-11 17:58:31 +0200

    Add logging

--- server/src/main.ts
+++ server/src/main.ts
@@ -14,7 +14,9 @@ import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
 
   const config = new DocumentBuilder()
       .setTitle('Cats example')
