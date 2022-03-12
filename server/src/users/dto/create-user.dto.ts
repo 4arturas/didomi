b35f8923830871d3268340f7927328d1f475052f
Art <4arturas@gmail.com>
2022-03-12 20:50:46 +0200

    Add api properties to CreateUserDto

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,19 +1,27 @@
 import {IsEmail, MinLength, MaxLength} from "class-validator";
+import {ApiProperty} from "@nestjs/swagger";
+import {Event} from "../../events/entities/event.entity";
 
 export class CreateUserDto {
 
 
+    @ApiProperty({ example: 'test@test.com', description: 'User email' })
     @IsEmail()
     @MinLength(5)
     @MaxLength(50)
     email: string;
 
+    @ApiProperty({ example: 'John', description: 'User first name' })
     @MinLength(2)
     @MaxLength(50)
     firstName: string;
 
+    @ApiProperty({ example: 'Doe', description: 'User last name' })
     @MinLength(2)
     @MaxLength(50)
     lastName: string;
+
+    @ApiProperty({ example: 'either \'sms_notifications\' or \'email_notifications\'', description: 'Consents' })
+    consents: Event[]
 
 }
