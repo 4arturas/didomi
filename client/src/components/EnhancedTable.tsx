069cd86a60f4bd773e844e5f2ab3a3ac70c94954
Art <4arturas@gmail.com>
2022-03-12 14:09:30 +0200

    Add EnhancedTable.tsx

---
+++ client/src/components/EnhancedTable.tsx
@@ -0,0 +1,234 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Paper from '@mui/material/Paper';
+import { visuallyHidden } from '@mui/utils';
+import {DefaultService, OpenAPI, ReturnUserDto} from "../services/openapi";
+import {useCallback, useEffect} from "react";
+import useApi from "../hooks/useApi";
+// import moment from "moment";
+
+
+function descendingComparator(a:any, b:any, orderBy:any) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order:any, orderBy:any) {
+    return order === 'desc'
+        ? (a:any, b:any) => descendingComparator(a, b, orderBy)
+        : (a:any, b:any) => -descendingComparator(a, b, orderBy);
+}
+
+// This method is created for cross-browser compatibility, if you don't
+// need to support IE11, you can use Array.prototype.sort() directly
+function stableSort(array:any, comparator:any) {
+    const stabilizedThis = array.map((el:any, index:any) => [el, index]);
+    stabilizedThis.sort((a:any, b:any) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) {
+            return order;
+        }
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el:any) => el[0]);
+}
+
+const headCells = [
+    // {
+    //     id: 'id',
+    //     numeric: false,
+    //     label: 'ID',
+    // },
+    {
+        id: 'firstName',
+        numeric: false,
+        label: 'Name',
+    },
+    {
+        id: 'email',
+        numeric: false,
+        label: 'Email',
+    },
+    {
+        id: 'consents',
+        numeric: false,
+        label: 'Consent is given for',
+    },
+];
+
+
+function EnhancedTableHead(props:any) {
+    const { order, orderBy, onRequestSort } = props;
+
+    const createSortHandler = (property:any) => (event:any) => {
+        onRequestSort(event, property);
+    };
+
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell/>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={'left'}
+                        padding={'normal'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    onRequestSort: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
+
+function EnhancedTable() {
+    const [rows, setRows] = React.useState<ReturnUserDto[]>([]);
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('email');
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleRequestSort = (event:any, property:any) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+
+    const handleChangePage = (event:any, newPage:number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event:any) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+    OpenAPI.BASE = 'http://localhost:4000';
+
+    const handleRequest = useCallback(async function <T>(request: Promise<T>) {
+
+        // setIsloading(true)
+        try {
+            const response = await request
+            // setError(undefined)
+            return response
+        } catch (error: any) {
+            // setError(error)
+        } finally {
+            // setIsloading(true)
+        }
+    }, []);
+
+    useEffect(()=> {
+        // useApi().handleRequest(DefaultService.usersControllerFindAll()).then( u => setRows(u) );
+        handleRequest(DefaultService.usersControllerFindAll()).then( (u: any) => setRows(u) );
+    }, []);
+
+    return (
+        <Box sx={{ width: '100%' }}>
+            <Paper sx={{ width: '100%', mb: 2 }}>
+                <TableContainer>
+                    <Table
+                        sx={{ minWidth: 750 }}
+                        aria-labelledby="tableTitle"
+                        size={'medium'}
+                    >
+                        <EnhancedTableHead
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                            rowCount={rows.length}
+                        />
+                        <TableBody>
+                            {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+                 rows.slice().sort(getComparator(order, orderBy)) */}
+                            {stableSort(rows, getComparator(order, orderBy))
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((row:any, index:number) => {
+                                    const labelId = `enhanced-table-checkbox-${index}`;
+
+                                    return (
+                                        <TableRow hover key={row.id}>
+                                            <TableCell>
+                                            </TableCell>
+                                            {/*<TableCell*/}
+                                            {/*    component="th"*/}
+                                            {/*    id={labelId}*/}
+                                            {/*    scope="row"*/}
+                                            {/*    padding="none"*/}
+                                            {/*>*/}
+                                            {/*    {row.id}*/}
+                                            {/*</TableCell>*/}
+                                            {/*<TableCell align="left">{moment(row.created_time).format(`YYYY-MM-DD HH:mm:ss`)}</TableCell>*/}
+                                            <TableCell align="left">{row.firstName} {row.lastName}</TableCell>
+                                            <TableCell align="left">{row.email}</TableCell>
+                                            <TableCell align="left">{row.consents.map( (e:any, index:number) => <span key={index}>{e.id}</span>)}</TableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            {emptyRows > 0 && (
+                                <TableRow
+                                    style={{
+                                        height: (53) * emptyRows,
+                                    }}
+                                >
+                                    <TableCell colSpan={6} />
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </Box>
+    );
+}
+
+export default EnhancedTable;
