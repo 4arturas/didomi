b3705c2b8684f1145fa6a3ee51309a7a62b34800
Art <4arturas@gmail.com>
2022-03-12 13:57:09 +0200

    Add menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, NavLink, Route, Routes} from 'react-router-dom'
-import {Grid, Menu, MenuItem} from "@mui/material";
-import PermIdentityIcon from "@mui/icons-material/PermIdentity";
-import Lists from "@mui/icons-material/ListAlt";
+import {Grid, MenuItem} from "@mui/material";
 import GiveConsent from "./components/GiveConsent";
 import Consents from "./components/Consents";
+import Paper from "@mui/material/Paper";
+import MenuList from "@mui/material/MenuList";
+import Stack from "@mui/material/Stack";
 
 function App() {
   return (
       <Router>
       <Grid container justifyContent="center">
           <Grid item xs={2}>
-            <Menu open>
-                <MenuItem>
-                    <NavLink to="/give-consent" className={({isActive}) => 'link' + (isActive ? ' activeLink' : '')}>
-                        {/*<PermIdentityIcon/>*/}
-                        <h2>Give consent</h2>
-                    </NavLink>
-                </MenuItem>
-                <MenuItem>
-                    <NavLink to="/consents" className={({isActive}) => 'link' + (isActive ? ' activeLink' : '')}>
-                        {/*<Lists/>*/}
-                        <h2>Collected consents</h2>
-                    </NavLink>
-                </MenuItem>
-            </Menu>
+              <Stack direction="row" spacing={2}>
+                  <Paper>
+                      <MenuList>
+                          <MenuItem>
+                              <NavLink to="/give-consent" className={({isActive}) => 'link' + (isActive ? ' activeLink' : '')}>
+                                <h2>Give consent</h2>
+                              </NavLink>
+                          </MenuItem>
+                          <MenuItem>
+                              <NavLink to="/consents" className={({isActive}) => 'link' + (isActive ? ' activeLink' : '')}>
+                                  <h2>Collected consents</h2>
+                              </NavLink>
+                          </MenuItem>
+                      </MenuList>
+                  </Paper>
+              </Stack>
+          </Grid>
             <Grid item xs={6} style={{margin: '20px'}}>
                 <Routes>
                     <Route path='/' element={<GiveConsent/>}/>
@@ -33,7 +37,6 @@ function App() {
                     <Route path='/consents' element={<Consents/>}/>
                 </Routes>
             </Grid>
-          </Grid>
       </Grid>
       </Router>
   );
