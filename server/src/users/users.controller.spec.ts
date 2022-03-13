ac23f3e817b5110463af5c8b6e59b984bd356096
Art <4arturas@gmail.com>
2022-03-13 20:32:06 +0200

    Trying to set up tests

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -1,20 +1,45 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import {User} from "./entities/user.entity";
+import { Repository} from "typeorm";
+import {UsersService} from "./users.service";
+import {INestApplication} from "@nestjs/common";
+import {Test, TestingModule} from "@nestjs/testing";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsersModule} from "./users.module";
+import {Event} from "../events/entities/event.entity";
 
-describe('UsersController', () => {
-  let controller: UsersController;
+describe('UserController (e2e)', () => {
+  let userService: UsersService;
+  let userRepository: Repository<User>;
+  let app: INestApplication;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-      providers: [UsersService],
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        UsersModule,
+        TypeOrmModule.forRoot({
+          type: "sqlite",
+          database: ':memory:',
+          synchronize: true,
+          entities: [User, Event]
+        }),
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    app = moduleFixture.createNestApplication();
+    await app.init();
+    userRepository = moduleFixture.get('UserRepository');
+    userService = new UsersService(userRepository);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  afterEach(async () => {
+    await userRepository.query('DELETE FROM users');
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userRepository).toBeDefined();
   });
 });
